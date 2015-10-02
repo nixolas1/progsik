db3865834d426806a7e8251b00eb2fc3bcffb0dd
Kasper Rynning-Tønnesen <kasper@kasperrt.no>
2015-10-02 17:37:38 +0200

    Made username unique, and fixed some database models

--- src/webapp/Sql.php
+++ src/webapp/Sql.php
@@ -17,9 +17,30 @@ class Sql
      */
     static function up()
     {
-        $q1 = "CREATE TABLE users (id INTEGER PRIMARY KEY, user VARCHAR(50), pass VARCHAR(50), email varchar(50) default null, fullname varchar(50), address varchar(50), postcode varchar (4), age varchar(50), bio varhar(50), isadmin INTEGER);";
-        $q6 = "CREATE TABLE posts (postId INTEGER PRIMARY KEY AUTOINCREMENT, author TEXT, title TEXT NOT NULL, content TEXT NOT NULL, date TEXT NOT NULL, FOREIGN KEY(author) REFERENCES users(user));";
-        $q7 = "CREATE TABLE comments(commentId INTEGER PRIMARY KEY AUTOINCREMENT, date TEXT NOT NULL, author TEXT NOT NULL, text INTEGER NOT NULL, belongs_to_post INTEGER NOT NULL, FOREIGN KEY(belongs_to_post) REFERENCES posts(postId));";
+        $q1 = "CREATE TABLE users (
+            id INTEGER PRIMARY KEY, 
+            user VARCHAR(50) NOT NULL UNIQUE, 
+            pass VARCHAR(50) NOT NULL, 
+            email varchar(50) default null, 
+            fullname varchar(50), 
+            address varchar(50), 
+            postcode varchar (4), 
+            age varchar(50), 
+            bio varhar(50), 
+            isadmin INTEGER);";
+        $q6 = "CREATE TABLE posts (
+            postId INTEGER PRIMARY KEY AUTOINCREMENT, 
+            author TEXT, title TEXT NOT NULL, 
+            content TEXT NOT NULL, 
+            date TEXT NOT NULL, 
+            FOREIGN KEY(author) REFERENCES users(user));";
+        $q7 = "CREATE TABLE comments(
+            commentId INTEGER PRIMARY KEY AUTOINCREMENT, 
+            date TEXT NOT NULL, 
+            author TEXT NOT NULL, 
+            text INTEGER NOT NULL, 
+            belongs_to_post INTEGER NOT NULL, 
+            FOREIGN KEY(belongs_to_post) REFERENCES posts(postId));";
 
         self::$pdo->exec($q1);
         self::$pdo->exec($q6);
