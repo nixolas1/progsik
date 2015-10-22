91a9c3affbfaba96a8065eafcf0fb04f9455936c
reaVen- <marnil37@gmail.com>
2015-10-22 17:47:06 +0200

    turned off error reporting

--- web/index.php
+++ web/index.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 'on');
+error_reporting(0);
+ini_set('display_errors', 'off');
 
 if (! extension_loaded('openssl')) {
     die('You must enable the openssl extension.');
